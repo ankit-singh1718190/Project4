60d3b6dbe978a41c51d80527598823fb8a14f56e
Ankit Singh <ankit1718190@gmail.com>
2024-11-03 10:27:55 +0530

    intila Commit

---
+++ myapp/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import ListView
+router = DefaultRouter()
+router.register('students', ListView, basename='student')
+urlpatterns = [
+    path('', include(router.urls)),
+]
